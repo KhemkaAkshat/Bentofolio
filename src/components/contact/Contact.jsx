878cf2bb8a2ae45f8968b27ce7c4f96d0f23d51f
KhemkaAkshat <144114211+KhemkaAkshat@users.noreply.github.com>
2024-07-22 23:38:56 +0530

    Made contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import "./contact.css";
 
 function Contact() {
   return (
-    <div>
-        Contact ME
+    <>
+    <div className='contact'>
+      <div className="leftBar">
+        <h3 className="inquiries">Inquiries:</h3>
+        <p>Please fill out the form on the right or email directly at </p>
+        <span>akshatkhemka07@gmail.com</span>
+      </div>
+      <div className="rightBar">
+        <p className="info">
+          Name*
+        </p>
+        <input type="text" />
+        <p className="info">
+          Email*
+        </p>
+        <input type="text" />
+        <p className="info">
+          Subject:
+        </p>
+        <input type="text" />
+        <p className="info">
+          Project Summary*
+        </p>
+        <textarea className="summary"></textarea>
+        <button className="send">send</button>
+      </div>
     </div>
+    </>
   )
 }
 
