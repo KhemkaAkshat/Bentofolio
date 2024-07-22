3c11b0da52b31c516650fad8d6882a370957477f
KhemkaAkshat <144114211+KhemkaAkshat@users.noreply.github.com>
2024-07-22 16:35:04 +0530

    fixed some basic issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './Home/Home'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <div>
       <Home/>
+      {/* <Contact/> */}
     </div>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import "./Contact.css";
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      contact page
+    </div>
+  )
+}
+
+export default Contact
